a987245174f481350948f4eb5355547a48f9c24e
EricGao-cn <148952552+EricGao-cn@users.noreply.github.com>
2025-02-28 16:15:55 +0800

    [feat] PartA finished (#1)

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,31 @@
 # =================================================================
 argmax:
     # Prologue
-
+    addi t0, x0, 1
+    bge a1, t0, loop_start
+    li a0, 36
+    j exit
 
 loop_start:
 
+    addi t0, x0, 0
+    lw t1, 0(a0)
+    addi t2, x0, 0
+
 
 loop_continue:
+    lw t3, 0(a0)
+    bge t1, t3, loop_branch
+    addi t0, t2, 0
+    addi t1, t3, 0
 
+loop_branch:
+    addi t2, t2, 1
+    addi a0, a0, 4
+    blt t2, a1, loop_continue
 
 loop_end:
     # Epilogue
+    mv a0, t0
 
     jr ra
--- src/relu.s
+++ src/relu.s
@@ -18,16 +18,24 @@ relu:
 
 loop_start:
 
+addi t0, x0, 1
+bge a0, t0, loop_continue
+li a0, 36
+j exit
 
-
-
-
-
-
+addi t0, x0, 1
 
 loop_continue:
 
+lw t1, 0(a0)
+bge t1, x0, loop_branch
+li t1, 0
+sw t1, 0(a0)
 
+loop_branch:
+addi t0, t0, 1
+addi a0, a0, 4
+blt t0, a1, loop_continue
 
 loop_end:
 
