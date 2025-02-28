678158a67635a3e70a840332cc9d5698d4341ffa
Eric Gao <gaoyuxuan.cn@gmail.com>
2025-02-28 16:13:31 +0800

    [feat] PartA finished

--- src/dot.s
+++ src/dot.s
@@ -20,25 +20,39 @@
 dot:
 
     # Prologue
+    addi t0, x0, 1
+    blt a2, t0, exp1
+    blt a3, t0, exp2
+    blt a4, t0, exp2
+    j loop_start
 
+exp1:
+    li a0, 36
+    j exit
+exp2:
+    li a0, 37
+    j exit
 
 loop_start:
+    addi t0, x0, 0  # result
+    addi t1, x0, 0  # index of arr0
+    slli t5, a3, 2  # step of add0
+    slli t6, a4, 2  # step of add1
 
-
-
-
-
-
-
-
-
-
-
+loop_body:
+    lw t3, 0(a0)  # load arr0[i]
+    lw t4, 0(a1)  # load arr1[i]
+    mul t3, t3, t4  # arr0[i] * arr1[i]
+    add t0, t0, t3  # result += arr0[i] * arr1[i]
+    add a0, a0, t5  # arr0 += stride0
+    add a1, a1, t6  # arr1 += stride1
+    addi t1, t1, 1 
+    blt t1, a2, loop_body
+    j loop_end
 
 loop_end:
 
-
     # Epilogue
-
+    mv a0, t0
 
     jr ra
